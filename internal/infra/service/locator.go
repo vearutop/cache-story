6bb4454dcd0781cd0908a1c2b7a915af2fbde93b
Viacheslav Poturaev <vearutop@gmail.com>
2022-05-10 02:00:47 +0200

    Fix CI

--- internal/infra/service/locator.go
+++ internal/infra/service/locator.go
@@ -2,14 +2,11 @@ package service
 
 import (
 	"github.com/bool64/brick"
-	"github.com/bool64/sqluct"
 )
 
 // Locator defines application resources.
 type Locator struct {
 	*brick.BaseLocator
-
-	Storage *sqluct.Storage
 
 	GreetingMakerProvider
 	GreetingClearerProvider
